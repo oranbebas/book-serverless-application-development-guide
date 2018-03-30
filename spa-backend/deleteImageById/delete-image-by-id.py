3da0521871861dc83c02b09efecc8aae974b21a8
oranbebas <oranbebas@gmail.com>
2018-03-30 16:35:39 +0900

    finish implementation API, p133

---
+++ spa-backend/deleteImageById/delete-image-by-id.py
@@ -0,0 +1,73 @@
+import boto3
+import uuid
+import json
+import logging
+import os
+import datetime
+from botocore.exceptions import ClientError
+from boto3.dynamodb.conditions import Key, Attr
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-1')
+table = dynamodb.Table(os.getenv('TABLE_NAME'))
+
+def lambda_handler(event, context):
+    try:
+        photo_id = event['pathParameters']['id']
+        try:
+            response = table.get_item(
+                Key={'photo_id': photo_id}
+            )
+
+            if 'Item' not in response:
+                logging.info("Specified key is not found.")
+                response = {
+                    'statusCode': '404',
+                    'body': 'Not Found',
+                    'headers': {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*'
+                    }
+                }
+                return response
+            else:
+                response = table.delete_item(
+                    Key={'photo_id': photo_id}
+                )
+        except ClientError as e:
+            logging.info(e.response['Error']['Message'])
+            response = {
+                'statusCode': '400',
+                'body': e.response['Error']['Message'],
+                'headers': {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                }
+            }
+            return response
+        else:
+            logging.info(
+                'Item (photo_id = ' + photo_id + ') is successfully deleted'
+            )
+            response = {
+                'statusCode': '204',
+                'body': '',
+                'headers': {
+                    'Content-Type': 'application/json'
+                }
+            }
+            return response
+    except Exception as e:
+        logging.error("type : %s", type(e))
+        logging.error(e)
